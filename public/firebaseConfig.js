55eb6e06f347e5691e85d5648c0ca8e8728bfa72
Maeva31 <121046388+Maeva31@users.noreply.github.com>
2025-04-19 16:35:54 +0200

    firebaseConfig.js

--- public/firebaseConfig.js
+++ public/firebaseConfig.js
@@ -1,11 +1,16 @@
-// Configuration Firebase (exemple)
-// Remplis avec tes propres infos
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
 const firebaseConfig = {
-    apiKey: "YOUR_API_KEY",
-    authDomain: "YOUR_AUTH_DOMAIN",
-    projectId: "YOUR_PROJECT_ID",
-    storageBucket: "YOUR_STORAGE_BUCKET",
-    messagingSenderId: "YOUR_SENDER_ID",
-    appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyC2pK1gUtspXbG4wDy6SdJmnivCco-t_zc",
+  authDomain: "chatapp-7ee15.firebaseapp.com",
+  projectId: "chatapp-7ee15",
+  storageBucket: "chatapp-7ee15.appspot.com",
+  messagingSenderId: "995598698289",
+  appId: "1:995598698289:web:580aaace2fa2b7beb89210"
 };
-firebase.initializeApp(firebaseConfig);
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+export { db };
