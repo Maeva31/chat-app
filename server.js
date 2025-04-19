b4f12981cdc57c8ea1ac6bd37e9a63eb9c5b66b1
Maeva31 <121046388+Maeva31@users.noreply.github.com>
2025-04-19 12:38:11 +0200

    server.js

--- server.js
+++ server.js
@@ -1,81 +1,71 @@
-const express = require('express');
-const http = require('http');
-const socketIo = require('socket.io');
 
-// Initialisation du serveur
+const express = require("express");
+const http = require("http");
+const path = require("path");
+const socketIo = require("socket.io");
+
 const app = express();
 const server = http.createServer(app);
 const io = socketIo(server);
 
-let channels = [
-  { name: 'Général', users: [], messages: [] },
-  { name: 'Musique', users: [], messages: [] },
-  { name: 'Gaming', users: [], messages: [] },
-  { name: 'Détente', users: [], messages: [] }
-];
+app.use(express.static(path.join(__dirname, "public")));
 
-let users = [];
+const users = {};
+const userChannels = {};
+const messageHistory = {};
+const defaultChannel = "# 💬 ┊ Général";
 
-app.use(express.static('public'));
-
-// Lorsque l'utilisateur se connecte
-io.on('connection', (socket) => {
-  console.log('Un utilisateur s\'est connecté');
-
-  // Gérer l'inscription de l'utilisateur
-  socket.on('userInfo', (userData) => {
-    users.push({ ...userData, socketId: socket.id });
-    io.emit('userList', users); // Mettre à jour la liste des utilisateurs
+io.on("connection", (socket) => {
+  socket.on("new-user", (data) => {
+    users[socket.id] = { ...data };
+    userChannels[socket.id] = data.channel || defaultChannel;
+    if (!messageHistory[userChannels[socket.id]]) {
+      messageHistory[userChannels[socket.id]] = [];
+    }
+    socket.join(userChannels[socket.id]);
+    io.to(userChannels[socket.id]).emit("user-list", getUsersInChannel(userChannels[socket.id]));
   });
 
-  // Gérer la création de salons
-  socket.on('createChannel', (channelName) => {
-    const newChannel = { name: channelName, users: [], messages: [] };
-    channels.push(newChannel);
-    io.emit('channelList', channels); // Mettre à jour la liste des salons
+  socket.on("chat-message", (data) => {
+    const channel = userChannels[socket.id];
+    const messageData = {
+      username: data.username,
+      message: data.message,
+      gender: data.gender,
+      age: data.age,
+      role: data.role,
+    };
+    messageHistory[channel].push(messageData);
+    io.to(channel).emit("chat-message", messageData);
   });
 
-  // Gérer l'envoi de messages
-  socket.on('sendMessage', (text) => {
-    const currentChannel = channels.find(channel => channel.users.includes(socket.id));
-    if (currentChannel) {
-      const message = { username: users.find(user => user.socketId === socket.id).username, text };
-      currentChannel.messages.push(message);
-      io.emit('message', message);
+  socket.on("create-channel", (channelName) => {
+    if (!messageHistory[channelName]) {
+      messageHistory[channelName] = [];
+      io.emit("channel-created", channelName);
     }
   });
 
-  // Gérer l'entrée dans un salon
-  socket.on('joinChannel', (channelName) => {
-    const channel = channels.find(ch => ch.name === channelName);
-    if (channel) {
-      channel.users.push(socket.id);
-      io.emit('userList', getUsersInChannel(channelName));
-      io.emit('message', { username: 'System', text: `${users.find(user => user.socketId === socket.id).username} a rejoint le salon ${channelName}` });
-    }
+  socket.on("switch-channel", ({ oldChannel, newChannel }) => {
+    socket.leave(oldChannel);
+    socket.join(newChannel);
+    userChannels[socket.id] = newChannel;
+    io.to(newChannel).emit("user-list", getUsersInChannel(newChannel));
   });
 
-  // Récupérer la liste des utilisateurs d'un salon
-  function getUsersInChannel(channelName) {
-    const channel = channels.find(ch => ch.name === channelName);
-    if (channel) {
-      return channel.users.map(userId => users.find(user => user.socketId === userId));
-    }
-    return [];
-  }
-
-  // Gérer la déconnexion d'un utilisateur
-  socket.on('disconnect', () => {
-    console.log('Un utilisateur s\'est déconnecté');
-    users = users.filter(user => user.socketId !== socket.id);
-    channels.forEach(channel => {
-      channel.users = channel.users.filter(userId => userId !== socket.id);
-    });
-    io.emit('userList', users);
+  socket.on("disconnect", () => {
+    const channel = userChannels[socket.id];
+    delete users[socket.id];
+    delete userChannels[socket.id];
+    io.to(channel).emit("user-list", getUsersInChannel(channel));
   });
 });
 
-// Démarrer le serveur sur le port 3000
-server.listen(3000, () => {
-  console.log('Serveur démarré sur http://localhost:3000');
-});
+function getUsersInChannel(channel) {
+  return Object.keys(users)
+    .filter((id) => userChannels[id] === channel)
+    .map((id) => users[id]);
+}
+
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => console.log(`Serveur lancé sur http://localhost:${PORT}`));
