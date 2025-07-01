622aae54051343c59ed69b114a864e5d0dc88eb9
Maeva31 <121046388+Maeva31@users.noreply.github.com>
2025-07-02 01:22:53 +0200

    webcamManager.js

--- webcamManager.js
+++ webcamManager.js
@@ -1,28 +1,28 @@
-// webcamManager.js
 export default function setupWebcam(io) {
   io.on('connection', (socket) => {
 
-    // Exemple : événement signaling pour WebRTC (offer, answer, candidate)
-    socket.on('webcam-offer', (data) => {
-      const { targetId, offer } = data;
-      io.to(targetId).emit('webcam-offer', { from: socket.id, offer });
+    socket.on('join-webcam-room', (room) => {
+      socket.join(room);
+      // Notifier les autres qu'un nouvel utilisateur est prêt
+      socket.to(room).emit('ready-for-call', { room, from: socket.id });
     });
 
-    socket.on('webcam-answer', (data) => {
-      const { targetId, answer } = data;
-      io.to(targetId).emit('webcam-answer', { from: socket.id, answer });
+    socket.on('leave-webcam-room', (room) => {
+      socket.leave(room);
+      // Notifier les autres que quelqu’un a quitté (optionnel)
     });
 
-    socket.on('webcam-candidate', (data) => {
-      const { targetId, candidate } = data;
-      io.to(targetId).emit('webcam-candidate', { from: socket.id, candidate });
+    socket.on('webrtc-offer', ({ offer, room, to }) => {
+      // Reçoit une offre et transmet au destinataire spécifique
+      io.to(to).emit('webrtc-offer', { offer, from: socket.id });
     });
 
-    socket.on('webcam-stop', () => {
-      // Notifier les autres que ce socket arrête sa webcam
-      socket.broadcast.emit('webcam-stopped', socket.id);
+    socket.on('webrtc-answer', ({ answer, room, to }) => {
+      io.to(to).emit('webrtc-answer', { answer, from: socket.id });
     });
 
-    // Tu peux gérer ici d'autres événements liés aux webcams.
+    socket.on('webrtc-ice-candidate', ({ candidate, room, to }) => {
+      io.to(to).emit('webrtc-ice-candidate', { candidate, from: socket.id });
+    });
   });
 }
