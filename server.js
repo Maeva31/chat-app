33ba5fc30319f70cff6034dc34b2296e4ea4301b
Maeva31 <121046388+Maeva31@users.noreply.github.com>
2025-07-01 13:10:55 +0200

    server.js

--- server.js
+++ server.js
@@ -530,7 +530,7 @@ io.on('connection', (socket) => {
 
 });
 
-const PORT = process.env.PORT || 1000;
+const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
   console.log(`🚀 Serveur démarré sur http://localhost:${PORT}`);
 });
