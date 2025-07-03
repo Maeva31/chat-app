c75e31bce1f7fdb375d3809c5aa7fffc3452f740
Maeva31 <121046388+Maeva31@users.noreply.github.com>
2025-07-03 13:48:39 +0200

    chatUtils.js

--- public/js/chatUtils.js
+++ public/js/chatUtils.js
@@ -18,11 +18,13 @@ export function getUsernameColor(gender) {
 }
 
 export function extractChannelName(text) {
+  if (typeof text !== 'string') return ''; // ou null selon usage
   text = text.replace(/\s*\(\d+\)$/, '').trim();
   const parts = text.split('┊');
   if (parts.length > 1) return parts[1].trim();
   return text.replace(/^#?\s*[\p{L}\p{N}\p{S}\p{P}\s]*/u, '').trim();
 }
+
 
 export function showBanner(message, type = 'error') {
   if (!window.initialLoadComplete) return;
