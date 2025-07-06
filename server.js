07e6d68dfb38cd9199b6d4b6a3252883d1367984
Maeva31 <121046388+Maeva31@users.noreply.github.com>
2025-07-06 19:08:51 +0200

    server.js

--- server.js
+++ server.js
@@ -33,8 +33,10 @@ setInterval(() => {
       if (Date.now() > expiration) {
         kicksMap.delete(username);
         console.log(`Kick expiré pour ${username} dans ${room}`);
-      }
-    }
+            }
+  }
+}, 60 * 1000); // par exemple toutes les minutes
+
 
 // Chargement des modérateurs
 let modData = { admins: [], modos: [] };
