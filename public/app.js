269535d18f360f0289cbfd3cd446f18c077d413f
Maeva31 <121046388+Maeva31@users.noreply.github.com>
2025-07-07 04:06:00 +0200

    app.js

--- public/app.js
+++ public/app.js
@@ -201,7 +201,7 @@ document.addEventListener('DOMContentLoaded', () => {
     const body = win.querySelector('.private-chat-body');
     appendPrivateMessage(body, from, message);
   });
-});
+
 
 
   
