865aae16c036ad3c45be08f08a546fee1e0e70b3
Maeva31 <121046388+Maeva31@users.noreply.github.com>
2025-07-09 13:50:23 +0200

    app.js

--- public/app.js
+++ public/app.js
@@ -944,35 +944,34 @@ const roleIconSpan = li.querySelector('.role-icon');
 const icon = createRoleIcon(role);
 if (icon) roleIconSpan.appendChild(icon);
 
-if (webcamActive) {
-  let camIcon = roleIconSpan.querySelector('.webcam-icon');
-  if (!camIcon) {
-    camIcon = document.createElement('img');
-    camIcon.src = '/webcam.gif';
-    camIcon.alt = 'Webcam active';
-    camIcon.title = 'Webcam active - cliquer pour voir';
-    camIcon.classList.add('webcam-icon');
-    camIcon.style.width = '16px';
-    camIcon.style.height = '16px';
-    camIcon.style.cursor = 'pointer';
-    camIcon.style.position = 'absolute';
-    camIcon.style.top = '0';
-    camIcon.style.left = '0';
-    camIcon.style.zIndex = '10';
-    roleIconSpan.style.position = 'relative';
+  if (webcamActive) {
+    let camIcon = roleIconSpan.querySelector('.webcam-icon');
+    if (!camIcon) {
+      camIcon = document.createElement('img');
+      camIcon.src = '/webcam.gif';
+      camIcon.alt = 'Webcam active';
+      camIcon.title = 'Webcam active - cliquer pour voir';
+      camIcon.classList.add('webcam-icon');
+      camIcon.style.width = '16px';
+      camIcon.style.height = '16px';
+      camIcon.style.cursor = 'pointer';
+      camIcon.style.position = 'absolute';
+      camIcon.style.top = '0';
+      camIcon.style.left = '0';
+      camIcon.style.zIndex = '10';
+      roleIconSpan.style.position = 'relative';
 
-    camIcon.addEventListener('click', () => {
-      console.log('Clic sur webcam de', username);
-      // Remplacer l'ouverture direct par l'appel de la fonction
-      openRemoteWebcamPopup(username);
-    });
+      camIcon.addEventListener('click', () => {
+        console.log('Clic sur webcam de', username);
+        openRemoteWebcamPopup(username);
+      });
 
-    roleIconSpan.appendChild(camIcon);
+      roleIconSpan.appendChild(camIcon);
+    }
+  } else {
+    const camIcon = roleIconSpan.querySelector('.webcam-icon');
+    if (camIcon) camIcon.remove();
   }
-} else {
-  const camIcon = roleIconSpan.querySelector('.webcam-icon');
-  if (camIcon) camIcon.remove();
-}
 
 
 
