68f662be8bb3b6704d1f36796fa6504f63bdc95a
Maeva31 <121046388+Maeva31@users.noreply.github.com>
2025-07-11 18:02:31 +0200

    app.js

--- public/app.js
+++ public/app.js
@@ -1733,7 +1733,10 @@ styleMenu.addEventListener('click', e => e.stopPropagation());
       font: styleFont.value
     };
     saveStyle(newStyle);
-    applyStyleToInput(newStyle);
+    Object.assign(currentStyle, newStyle);  // Met à jour currentStyle
+
+    applyStyleToInput(document.getElementById('message-input'), currentStyle);
+    updateAllInputStyles();  // Met à jour tous les inputs privés
   });
 });
 
