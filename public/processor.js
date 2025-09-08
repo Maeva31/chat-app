12492fc167afd0c3d5ad5836897b3ea2235ea774
Maeva31 <121046388+Maeva31@users.noreply.github.com>
2025-09-08 07:43:22 +0200

    Update processor.js

--- public/processor.js
+++ public/processor.js
@@ -1,14 +1,27 @@
 class MicProcessor extends AudioWorkletProcessor {
+  constructor() {
+    super();
+  }
+
   process(inputs, outputs, parameters) {
     const input = inputs[0];
     const output = outputs[0];
 
-    // Copier le signal d’entrée vers la sortie
-    for (let channel = 0; channel < input.length; channel++) {
-      output[channel].set(input[channel]);
+    if (input && input.length > 0) {
+      let sum = 0;
+      for (let channel = 0; channel < input.length; channel++) {
+        const inputChannel = input[channel];
+        const outputChannel = output[channel];
+        for (let i = 0; i < inputChannel.length; i++) {
+          outputChannel[i] = inputChannel[i]; // passthrough
+          sum += inputChannel[i] ** 2;
+        }
+      }
+
+      const rms = Math.sqrt(sum / input[0].length);
+      this.port.postMessage({ volume: rms });
     }
 
-    // 🔊 Ici tu peux analyser ou modifier le son avant envoi
     return true;
   }
 }
