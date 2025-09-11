6cd413e2d59af5ebf1e032bc5bc5631f5c57e4cd
Maeva31 <121046388+Maeva31@users.noreply.github.com>
2025-09-11 14:03:26 +0200

    Update processor.js

--- public/processor.js
+++ public/processor.js
@@ -1,29 +1,41 @@
-class MicProcessor extends AudioWorkletProcessor {
+class ChatProcessor extends AudioWorkletProcessor {
+  static get parameterDescriptors() {
+    return [
+      {
+        name: "cutoff",
+        defaultValue: 150, // 150 Hz = coupe les bruits graves
+        minValue: 20,
+        maxValue: 1000,
+      }
+    ];
+  }
+
   constructor() {
     super();
+    this.lastSample = 0; // mémoire pour le filtre
   }
 
   process(inputs, outputs, parameters) {
     const input = inputs[0];
     const output = outputs[0];
+    const cutoff = parameters.cutoff[0]; 
 
-    if (input && input.length > 0) {
-      let sum = 0;
-      for (let channel = 0; channel < input.length; channel++) {
-        const inputChannel = input[channel];
-        const outputChannel = output[channel];
-        for (let i = 0; i < inputChannel.length; i++) {
-          outputChannel[i] = inputChannel[i]; // passthrough
-          sum += inputChannel[i] ** 2;
-        }
+    if (input && input[0]) {
+      const inChan = input[0];
+      const outChan = output[0];
+      const RC = 1.0 / (cutoff * 2 * Math.PI);
+      const dt = 1.0 / sampleRate;
+      const alpha = RC / (RC + dt);
+
+      for (let i = 0; i < inChan.length; i++) {
+        // filtre passe-haut simple
+        const filtered = alpha * (this.lastSample + inChan[i] - (i > 0 ? inChan[i - 1] : this.lastSample));
+        outChan[i] = filtered;
+        this.lastSample = inChan[i];
       }
-
-      const rms = Math.sqrt(sum / input[0].length);
-      this.port.postMessage({ volume: rms });
     }
-
     return true;
   }
 }
 
-registerProcessor('mic-processor', MicProcessor);
+registerProcessor("chat-processor", ChatProcessor);
